99876463626cba6df81e7f426e9444a5b884f20d
Stanislav <stanislav_kharchenko@outlook.com>
2023-06-09 01:05:10 +0200

    Fexed Links

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -46,8 +46,6 @@ const ProjectCard = ({
                                     className='w-1/2 h-1/2 object-contain'
                                 />
                             </div>
-                        </div>
-                        <div className='absolute inset-0 flex justify-start m-3 card-img_hover'>
                             <div
                                 onClick={() => window.open(site_link, "_blank")}
                                 className='black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer'
